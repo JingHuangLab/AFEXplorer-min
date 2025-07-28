ad349eda68130a2ee5cd38365c58b577182a2b9c
Song Zilin <song.zilin@outlook.com>
2025-07-28 11:42:26 +0800

    lol.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+import jax.numpy as jnp
 import afex.model_multimer  as _m
 import afex.runner_multimer as _r
 
@@ -9,6 +9,11 @@ runner = _r.AFEXMultimerRunner(work_dir='./run0',
                                feat_dir='./run0/features.pkl', 
                                afex_config=_m.AFEXMultimerConfig(model_name=model_name), 
                                afex_params=_m.AFEXMultimerParams(model_name=model_name), )
+
+def colvar(pos: jnp.ndarray) -> jnp.ndarray:
+  r"""pos - [Nres, 37 ,3]."""
+
+
 
 runner.execute(colvar_fn=lambda x: 0., )
 
